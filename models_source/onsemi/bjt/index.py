18800f22a3973c3d85ee2a2fdbcef4a1ab5a3f24
Christer Weinigel <christer@weinigel.se>
2018-08-29 21:24:51 +0200

    Import ON Semi BJT transistor models

---
+++ models_source/onsemi/bjt/index.py
@@ -0,0 +1,9 @@
+# 50A02CH-SPICE/D Rev 1, May 2016
+# http://www.onsemi.com/pub/Collateral/50A02CH-SPICE-D.ZIP
+Model('50A02CH',
+      mod = ('50A02CH-SPICE-D.txt', 'ac6860b2634f5c478f6d'))
+
+# 50C02CH_SPICE/D Rev 1, May 2016
+# http://www.onsemi.com/pub/Collateral/50C02CH_SPICE-D.LIB
+Model('50C02CH',
+      mod = ('50C02CH_SPICE-D.LIB', '2cab42bd966cbedd22fb'))
