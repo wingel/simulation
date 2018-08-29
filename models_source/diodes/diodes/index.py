49d381793188359b73efc82812d5375d33a5f23f
Christer Weinigel <christer@weinigel.se>
2018-08-29 21:54:28 +0200

    Patch BAV99 model

--- models_source/diodes/diodes/index.py
+++ models_source/diodes/diodes/index.py
@@ -1,3 +1,5 @@
 # BAV99 SPICE Model
 # https://www.diodes.com/products/discrete/diodes-and-rectifiers/diodes/small-signal-switching-diodes/part/BAV99
-Model('BAV99', mod = ('BAV99.spice.txt', 'a28d85c90c3ff2674a3c'))
+Model('BAV99',
+      mod = Patch('BAV99.spice.txt', 'BAV99.spice.diff',
+                  '41628457630847fc81f1'))
