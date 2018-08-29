aab43f5b9afd3488469dbb7097e15771fdadb236
Christer Weinigel <christer@weinigel.se>
2018-08-29 20:19:52 +0200

    Import Diodes Incorporated BAV99 model

---
+++ models_source/diodes/diodes/index.py
@@ -0,0 +1,3 @@
+# BAV99 SPICE Model
+# https://www.diodes.com/products/discrete/diodes-and-rectifiers/diodes/small-signal-switching-diodes/part/BAV99
+Model('BAV99', mod = ('BAV99.spice.txt', 'a28d85c90c3ff2674a3c'))
